c1af0798db4bb43da7739a2b98c2dfdb0ba3b944
Taani Maama <taani1maama@live.com>
2021-08-01 12:28:04 -0700

    Finished Base User model tests.

--- server/modules/user/useCases/createUser/createUser.spec.ts
+++ server/modules/user/useCases/createUser/createUser.spec.ts
@@ -31,4 +31,70 @@ describe("When I create a user...", () => {
       expect(user).to.not.be.null;
     });
   });
+
+  describe("with the following invalid credentials...", () => {
+    it("No first name", async () => {
+      try {
+        const userObject: IBaseUser = {
+          firstName: "",
+          lastName: "test",
+          email: "test@test.com",
+          password: "testTest123$"
+        };
+
+        const user = await db.BaseUser.create(userObject);
+        expect(user).to.be.null;
+      } catch (error) {
+        expect(error).to.not.be.null;
+      }
+    });
+
+    it("No last name", async () => {
+      try {
+        const userObject: IBaseUser = {
+          firstName: "test",
+          lastName: "",
+          email: "test@test.com",
+          password: "testTest123$"
+        };
+
+        const user = await db.BaseUser.create(userObject);
+        expect(user).to.be.null;
+      } catch (error) {
+        expect(error).to.not.be.null;
+      }
+    });
+
+    it("A invalid email", async () => {
+      try {
+        const userObject: IBaseUser = {
+          firstName: "test",
+          lastName: "test",
+          email: "testemail",
+          password: "testTest123$"
+        };
+
+        const user = await db.BaseUser.create(userObject);
+        expect(user).to.be.null;
+      } catch (error) {
+        expect(error).to.not.be.null;
+      }
+    });
+
+    it("A invalid password", async () => {
+      try {
+        const userObject: IBaseUser = {
+          firstName: "test",
+          lastName: "test",
+          email: "testemail",
+          password: "password"
+        };
+
+        const user = await db.BaseUser.create(userObject);
+        expect(user).to.be.null;
+      } catch (error) {
+        expect(error).to.not.be.null;
+      }
+    });
+  });
 });
